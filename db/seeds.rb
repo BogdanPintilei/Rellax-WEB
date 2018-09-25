2ea437d38a8896653a91fd00b38fed30175ad381
Rares Abrudan <raresabr@gmail.com>
2018-09-25 09:39:53 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Track.create([
+               { track_name: 'Track', track_description: 'Description', track_image: 'https://idk', track_audio: 'https://idk2',
+                 track_metering_levels: [1, 2, 3, 4, 5] },
+               { track_name: 'Track', track_description: 'Description', track_image: 'https://idk', track_audio: 'https://idk2',
+                 track_metering_levels: [1, 2, 3, 4, 5] },
+               { track_name: 'Track', track_description: 'Description', track_image: 'https://idk', track_audio: 'https://idk2',
+                 track_metering_levels: [1, 2, 3, 4, 5] },
+               { track_name: 'Track', track_description: 'Description', track_image: 'https://idk', track_audio: 'https://idk2',
+                 track_metering_levels: [1, 2, 3, 4, 5] }
+             ])
